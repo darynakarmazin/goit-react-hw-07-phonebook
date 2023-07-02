b8386f8c7ad51ebb8a5b2bce3104344b14f60ae7
Daryna Karmazin <daryna.karmazin@gmail.com>
2023-07-02 22:29:33 +0300

    Added useEffect to App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import { Wrapper, Header } from './App.styled';
 import { ContactForm } from './contactForm/ContactForm';
@@ -6,30 +6,34 @@ import { Filter } from './filter/Filter';
 import { ContactList } from './contactList/ContactList';
 import { useDispatch, useSelector } from 'react-redux';
 import { setFilter } from 'redux/filterSlice';
-import { addedContact, deletedContact } from 'redux/contactsSlice';
 import { getСontacts, getFilter } from 'redux/selectors';
+import { fetchContacts } from 'redux/operations';
 
 export function App() {
   const dispatch = useDispatch();
   const contacts = useSelector(getСontacts);
   const filter = useSelector(getFilter);
 
-  const deleteContact = contactId => {
-    dispatch(deletedContact(contactId));
-  };
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
 
-  const addContact = (name, number) => {
-    if (
-      contacts.find(
-        contact => contact.name.toLowerCase() === name.toLowerCase()
-      )
-    ) {
-      alert(`${name} is already in contacts!`);
-      return;
-    }
+  // const deleteContact = contactId => {
+  //   dispatch(deletedContact(contactId));
+  // };
 
-    dispatch(addedContact(name, number));
-  };
+  // const addContact = (name, number) => {
+  //   if (
+  //     contacts.find(
+  //       contact => contact.name.toLowerCase() === name.toLowerCase()
+  //     )
+  //   ) {
+  //     alert(`${name} is already in contacts!`);
+  //     return;
+  //   }
+
+  //   dispatch(addedContact(name, number));
+  // };
 
   const changeFilter = event => dispatch(setFilter(event.currentTarget.value));
 
@@ -44,13 +48,15 @@ export function App() {
   return (
     <Wrapper>
       <Header>Phonebook</Header>
-      <ContactForm onSubmit={addContact} />
+      <ContactForm
+      // onSubmit={addContact}
+      />
 
       <h2>Contacts</h2>
       <Filter value={filter} onChange={changeFilter} />
       <ContactList
         filtredContacts={filtredContacts}
-        onDeleteContact={deleteContact}
+        // onDeleteContact={deleteContact}
       />
     </Wrapper>
   );
