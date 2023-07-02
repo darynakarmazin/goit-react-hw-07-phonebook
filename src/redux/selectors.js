923320b1d879c9c3f4aa9f6095a516165e45c8b8
Daryna Karmazin <daryna.karmazin@gmail.com>
2023-07-02 22:14:05 +0300

    Added new selectors

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,2 +1,5 @@
-export const getСontacts = state => state.contacts;
+export const getСontacts = state => state.contacts.items;
+export const getIsLoading = state => state.tasks.isLoading;
+export const getError = state => state.tasks.error;
+
 export const getFilter = state => state.filter;
