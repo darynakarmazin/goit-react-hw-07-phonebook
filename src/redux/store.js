f6f275b8ba640020b75732cd37664dbb428de056
Daryna Karmazin <daryna.karmazin@gmail.com>
2023-07-02 22:14:25 +0300

    Updated store (remove persist)

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,33 +1,10 @@
-import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import { contactsReducer } from './contactsSlice';
 import { filterReducer } from './filterSlice';
-import { CONTACTS } from 'redux/constants';
-
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-
-// export const store = configureStore({
-//   reducer: {
-//     contacts: contactsReducer,
-//     filter: filterReducer,
-//   },
-// });
-
-const persistConfig = {
-  key: 'root',
-  storage,
-  whitelist: [CONTACTS],
-};
-
-const rootReducer = combineReducers({
-  contacts: contactsReducer,
-  filter: filterReducer,
-});
-
-const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: persistedReducer,
+  reducer: {
+    contacts: contactsReducer,
+    filter: filterReducer,
+  },
 });
-
-export const persistor = persistStore(store);
